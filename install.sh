8240f87ec75a2a8aefe9a01d6168a15be6644f35
Agustianes U. S <anezch@gmail.com>
2020-12-18 09:36:09 +0700

    Add i3 config

--- install.sh
+++ install.sh
@@ -4,19 +4,19 @@ mypath="$(dirname "`readlink -f "$0"`")"
 
 
 function create_link () {
-    source=$1
+    src=$1
     dest=$2
 
-    source_path="${mypath}/${source}"
+    source_path="${mypath}/${src}"
     dest_path="${HOME}/${dest}"
 
     if [[ -e "${dest_path}" ]]; then
         echo "${dest_path} already exist"
-        exit 1
+        return 1
     fi
 
     ln -vs "${source_path}" "${dest_path}"
 }
 
-
 create_link dot-config/nvim .config/nvim
+create_link dot-config/i3 .config/i3
