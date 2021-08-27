5e38639e95b4998fb8f0071f0b3377b8035dea1a
Agustianes Umbara Suwardi <anezch@gmail.com>
2021-08-28 01:21:02 +0700

    Basic usability
    - Ivy
    - Counsel
    - Which-key
    - Doom modeline
    - Doom themes

--- emacs/.aumacs/init.el
+++ emacs/.aumacs/init.el
@@ -11,7 +11,7 @@
 ;; Set up visible bell
 (setq visible-bell t)
 
-(set-face-attribute 'default nil :font "JetBrains Mono" :height 110)
+(set-face-attribute 'default nil :font "Iosevka SS10" :height 100)
 
 (load-theme 'tango-dark t)
 
@@ -40,7 +40,8 @@
 
 ;; Set up Ivy
 (use-package ivy
-  :diminish
+  :diminish (ivy-mode . "")
+  :init (ivy-mode 1)
   :bind (("C-s" . swiper)
 	 :map ivy-minibuffer-map
 	 ("TAB" . ivy-alt-done)
@@ -55,4 +56,41 @@
 	 ("C-k" . ivy-previous-line)
 	 ("C-d" . ivy-reverse-i-search-kill))
   :config
-  (ivy-mode 1))
+  (setq ivy-user-virtual-buffers t))
+
+(use-package counsel
+  :bind*
+  (("M-x"      . counsel-M-x)))
+
+(use-package which-key
+  :init
+  (which-key-mode)
+  :config
+  (which-key-setup-side-window-right-bottom)
+  (setq which-key-sort-order 'which-key-key-order-alpha
+	which-key-side-window-max-width 0.33
+	which-key-idle-delay 0.05)
+  :diminish which-key-mode)
+
+(use-package all-the-icons)
+
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 28))
+  :config
+  (set-face-attribute 'mode-line nil :family "Envy Code R" :height 100)
+  (set-face-attribute 'mode-line-inactive nil :family "Envy Code R" :height 100))
+
+(use-package doom-themes
+  :ensure t
+  :config
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold t
+	doom-themes-enable-italic t)
+  (load-theme 'doom-gruvbox t)
+
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
