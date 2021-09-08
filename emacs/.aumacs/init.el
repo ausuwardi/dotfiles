6ac722ee132f42534f1273119fd7306075bd9134
Agustianes Umbara Suwardi <anezch@gmail.com>
2021-09-09 02:06:31 +0700

    aumacs - org mode
    - org
    - org-roam

--- emacs/.aumacs/init.el
+++ emacs/.aumacs/init.el
@@ -193,6 +193,64 @@
   :custom
   (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
 
+(use-package org
+  :ensure t
+  :custom
+  (org-log-done t)
+  (org-agenda-files (list "~/org/gtd.org"))
+  (org-src-fontify-natively t)
+  (org-src-tab-acts-natively t)
+  (org-babel-default-header-args '((:eval . "never-export")))
+  (org-ascii-headline-spacing (quote (1 . 1)))
+  (org-startup-indented t)
+  (org-pretty-entities t)
+  (org-hide-emphasis-markers t)
+  (org-startup-with-inline-images t)
+  (org-image-actual-width '(300))
+  :hook
+  (org-mode . variable-pitch-mode)
+  (org-mode . visual-line-mode)
+  (org-mode . org-indent-mode)
+  :config
+  (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '(
+     (emacs-lisp . t)
+     (org . t)
+     (shell . t)
+     (python . t)
+     (awk . t)
+     ))
+  :bind (("C-c l" . org-store-link)
+	 ("C-c a" . org-agenda)))
+
+(use-package org-bullets
+  :config
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+
+(use-package org-autolist
+  :config
+  (add-hook 'org-mode-hook (lambda () (org-autolist-mode 1))))
+
+(use-package org-roam
+  :ensure t
+  :custom
+  (org-roam-v2-ack t)
+  (org-roam-directory (file-truename "~/org-roam/"))
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+	 ("C-c n f" . org-roam-node-find)
+	 ("C-c n i" . org-roam-node-insert)
+	 ("C-c n g" . org-roam-graph)
+	 ("C-c n c" . org-roam-capture)
+	 ;; Dailies
+	 ("C-c n d" . org-roam-dailies-goto-today)
+	 ("C-c n j" . org-roam-dailies-capture-today))
+  :config
+  (org-roam-db-autosync-mode))
+
+(use-package org-tree-slide)
+
 ;; Editing stuff
 ;; Line numbers
 (column-number-mode)
@@ -203,3 +261,42 @@
 		shell-mode-hook
 		eshell-mode-hook))
   (add-hook mode (lambda () (display-line-numbers-mode 0))))
+;; Highlight current line
+(global-hl-line-mode 1)
+
+(let* ((variable-tuple
+	(cond ((x-list-fonts "Fira Sans")      '(:font "Fira Sans"))
+	      ((x-list-fonts "Noto Sans")         '(:font "Noto Sans"))
+	      ((x-list-fonts "Roboto")      '(:font "Roboto"))
+	      ((x-list-fonts "Sans Serif")     '(:family "Sans Serif"))
+	      (nil (warn "Cannot find a Sans Serif Font. Install Fira Sans."))))
+       (base-font-color (face-foreground 'default nil 'default))
+       (headline       `(:inherit default :weight bold :foreground, base-font-color)))
+  (custom-theme-set-faces
+   'user
+   `(variable-pitch ((t (:family "Noto Sans" :height 100 :weight medium))))
+   `(fixed-pitch ((t (:family "Iosevka SS10" :height 100))))
+   `(org-block ((t (:inherit fixed-pitch))))
+   `(org-code ((t (:inherit (shadow fixed-pitch)))))
+   `(org-document-info ((t (:foreground "dark-orange"))))
+   `(org-document-info-keyword ((t (:inherit (shadow fixed-pitch)))))
+   `(org-indent ((t (:inherit (org-hide fixed-pitch)))))
+   `(org-link ((t (:foreground "royal blue" :underline t))))
+   `(org-meta-line ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+   `(org-property-value ((t (:inherit fixed-pitch))) t)
+   `(org-special-keyword ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+   `(org-table ((t (:inherit fixed-pitch))))
+   `(org-tag ((t (:inherit (shadow fixed-pitch) :weight bold :height 0.8))))
+   `(org-verbatim ((t (:inherit (shadow fixed-pitch)))))
+   `(org-level-8 ((t (,@headline ,@variable-tuple))))
+   `(org-level-7 ((t (,@headline ,@variable-tuple))))
+   `(org-level-6 ((t (,@headline ,@variable-tuple))))
+   `(org-level-5 ((t (,@headline ,@variable-tuple))))
+   `(org-level-4 ((t (,@headline ,@variable-tuple :height 1.1))))
+   `(org-level-3 ((t (,@headline ,@variable-tuple :height 1.2))))
+   `(org-level-2 ((t (,@headline ,@variable-tuple :height 1.3))))
+   `(org-level-1 ((t (,@headline ,@variable-tuple :height 1.4))))
+   `(org-document-title ((t (,@headline ,@variable-tuple :height 1.6 :underline nil))))))
+
+;; References
+;; https://zzamboni.org/post/beautifying-org-mode-in-emacs/
