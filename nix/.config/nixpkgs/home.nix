14a8fe3f986a1bb9fb6478def3ddd73d81369f6b
Agustianes U. S <anezch@gmail.com>
2023-05-31 11:12:21 +0700

    Tmux - Seamless pane switching with Vim/Neovim
    Requires configuration in Vim/Neovim

--- nix/.config/nixpkgs/home.nix
+++ nix/.config/nixpkgs/home.nix
@@ -82,35 +82,51 @@
   programs.tmux = {
     enable = true;
     terminal = "screen-256color";
+    keyMode = "vi";
+    baseIndex = 1;
+    plugins = with pkgs; [
+      tmuxPlugins.yank
+      tmuxPlugins.tmux-fzf
+      tmuxPlugins.gruvbox
+    ];
     extraConfig = ''
       set-option -g focus-events on
       set-option -sa terminal-overrides ",XXX:RGB"
 
+      # Vim style pane selection
+      bind h select-pane -L
+      bind j select-pane -D
+      bind k select-pane -U
+      bind l select-pane -R
+
+      # Seamless pane switching with (N)Vim and FZF
       is_vim="ps -o state= -o comm= -t '#{pane_tty}' | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?g?(view|n?vim?x?)(diff)?$'"
-
       is_fzf="ps -o state= -o comm= -t '#{pane_tty}' | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?fzf$'"
-
       bind -n C-h run "($is_vim && tmux send-keys C-h) || tmux select-pane -L"
-
       bind -n C-j run "($is_vim && tmux send-keys C-j) || ($is_fzf && tmux send-keys C-j) || tmux select-pane -D"
-
       bind -n C-k run "($is_vim && tmux send-keys C-k) || ($is_fzf && tmux send-keys C-k) || tmux select-pane -U"
-
       bind -n C-l run  "($is_vim && tmux send-keys C-l) || tmux select-pane -R"
-
       tmux_version='$(tmux -V | sed -En "s/^tmux ([0-9]+(.[0-9]+)?).*/\1/p")'
-
       if-shell -b '[ "$(echo "$tmux_version < 3.0" | bc)" = 1 ]' "bind-key -n 'C-\\' if-shell \"$is_vim\" 'send-keys C-\\'  'select-pane -l'"
       if-shell -b '[ "$(echo "$tmux_version >= 3.0" | bc)" = 1 ]' "bind-key -n 'C-\\' if-shell \"$is_vim\" 'send-keys C-\\\\'  'select-pane -l'"
-
       bind-key -n 'C-Space' if-shell "$is_vim" 'send-keys C-Space' 'select-pane -t:.+'
 
+      # Pane switchig while in copy mode
       bind-key -T copy-mode-vi 'C-h' select-pane -L
       bind-key -T copy-mode-vi 'C-j' select-pane -D
       bind-key -T copy-mode-vi 'C-k' select-pane -U
       bind-key -T copy-mode-vi 'C-l' select-pane -R
       bind-key -T copy-mode-vi 'C-\' select-pane -l
       bind-key -T copy-mode-vi 'C-Space' select-pane -t:.+
+
+      # Vi style copy mode
+      bind-key -T copy-mode-vi v send-keys -X begin-selection
+      bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
+      bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
+
+      # Set CWD when splitting
+      bind '"' split-window -v -c "#{pane_current_path}"
+      bind % split-window -h -c "#{pane_current_path}"
     '';
   };
 
